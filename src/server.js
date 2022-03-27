64149754c4392942bf8e2cdc52f9ab83ab095d46
ruanvsrateira <101360876+ruanvsrateira@users.noreply.github.com>
2022-03-27 14:44:01 -0300

    change port

--- src/server.js
+++ src/server.js
@@ -8,4 +8,4 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(3000, () => console.log('servidor rodando ...'))
+app.listen(3333, () => console.log('servidor rodando ...'))
